3e4ded91b1273dd5d034a66ccc45a496dac30b20
Adam Mescher <admescher@gmail.com>
2019-10-31 15:21:16 -0600

    Remove misplaced styles from Graph and into SimpleText component

--- components/Graph/styled.js
+++ components/Graph/styled.js
@@ -1,17 +1,5 @@
 import styled from 'styled-components';
 
-const StyledGraph = styled.div`
-  height: '100%';
-  width: '100%';
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  p {
-    font-family: sans-serif;
-    font-size: 10rem;
-    font-weight: bold;
-    color: #4F81BD;
-  }
-`;
+const StyledGraph = styled.div``;
 
 export default StyledGraph;
--- components/SimpleText/styled.js
+++ components/SimpleText/styled.js
@@ -1,5 +1,17 @@
 import styled from 'styled-components';
 
-const StyledSimpleText = styled.div``;
+const StyledSimpleText = styled.div`
+  height: '100%';
+  width: '100%';
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p {
+    font-family: sans-serif;
+    font-size: 10rem;
+    font-weight: bold;
+    color: #4F81BD;
+  }
+`;
 
 export default StyledSimpleText;
