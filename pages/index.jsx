298d62a175db0dee6b8a60e80498f55479ccb8ad
Adam Mescher <admescher@gmail.com>
2019-10-31 13:25:03 -0600

    Rename Home page to Index and update import from scatterplot to graph

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,11 @@
-const Home = () => {
-  return <div>Next.js Test</div>;
+import highcharts from 'highcharts';
+import Graph from '../components/Graph/index';
+import scatterplotOptions from '../lib/scatterplotOptions';
+
+const Index = () => {
+  return (
+    <Graph highcharts={highcharts} options={scatterplotOptions}/>
+  )
 }
 
-export default Home;
+export default Index;
