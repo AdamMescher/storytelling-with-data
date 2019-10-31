f77d24dc8dcc905722ec252066da626ca4518229
Adam Mescher <admescher@gmail.com>
2019-10-31 15:03:53 -0600

    Fix linting errors in Meta component

--- components/Meta/index.jsx
+++ components/Meta/index.jsx
@@ -7,8 +7,8 @@ const Meta = () => (
     <meta name="author" content="Adam Mescher" />
     <meta name="description" content="Storytelling with Data graph recreation with Highcharts" />
     <title>SWD</title>
-    <style>{`body { margin: 0; }`}</style>
-    </Head>
+    <style>{'body { margin: 0; }'}</style>
+  </Head>
 );
 
 export default Meta;
