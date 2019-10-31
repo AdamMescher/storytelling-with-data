a212bc65df14743547e5ece2cf96e53e476b7d18
Adam Mescher <admescher@gmail.com>
2019-10-31 11:27:27 -0600

    Create components directory and create Scatterplot component

---
+++ components/Scatterplot/index.jsx
@@ -0,0 +1,7 @@
+import StyledScatterplot from './styled';
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
+const Scatterplot = () => {
+    <HighchartsReact highcharts={} options={} />
+}
---
+++ components/Scatterplot/styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const StyledScatterplot = styled.div``;
+
+export default StyledScatterplot;
