0aa8ca658b87f000072c89ec2fc4e7778870342b
Adam Mescher <admescher@gmail.com>
2019-10-31 14:48:41 -0600

    Add main tag and call SimpleText on Index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,17 @@
 import highcharts from 'highcharts';
 import Graph from '../components/Graph/index';
+import SimpleText from '../components/SimpleText/index';
 import scatterplotOptions from '../lib/scatterplotOptions';
 
 const Index = () => (
-  <section>
-    <Graph highcharts={highcharts} options={scatterplotOptions} />
-  </section>
+  <main>
+    <section>
+      <Graph highcharts={highcharts} options={scatterplotOptions} />
+    </section>
+    <section>
+      <SimpleText text={'91%'} />
+    </section>
+  </main>
 );
 
 export default Index;
