6d148dd6fb905c000f9ed11b70f02685f4e3efa0
Adam Mescher <admescher@gmail.com>
2019-10-31 13:44:20 -0600

    Edit formatting of Index to fix lint errors

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,10 @@ import highcharts from 'highcharts';
 import Graph from '../components/Graph/index';
 import scatterplotOptions from '../lib/scatterplotOptions';
 
-const Index = () => {
-  return (
-    <Graph highcharts={highcharts} options={scatterplotOptions}/>
-  )
-}
+const Index = () => (
+  <section>
+    <Graph highcharts={highcharts} options={scatterplotOptions} />
+  </section>
+);
 
 export default Index;
