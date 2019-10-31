43781900d4750b63b6cbf3e725671abafc86a781
Adam Mescher <admescher@gmail.com>
2019-10-31 15:02:21 -0600

    Create Meta component

---
+++ components/Meta/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <meta name="author" content="Adam Mescher" />
+    <meta name="description" content="Storytelling with Data graph recreation with Highcharts" />
+    <title>SWD</title>
+    <style>{`body { margin: 0; }`}</style>
+    </Head>
+);
+
+export default Meta;
