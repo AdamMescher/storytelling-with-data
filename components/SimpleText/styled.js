ff5ff4d2b65795bb0e4e789afe660f5a052b1bcb
Adam Mescher <admescher@gmail.com>
2019-11-03 16:06:58 -0700

    Remove unnecessary height and width properties and update font-size to vw

--- components/SimpleText/styled.js
+++ components/SimpleText/styled.js
@@ -1,14 +1,12 @@
 import styled from 'styled-components';
 
 const StyledSimpleText = styled.div`
-  height: '100%';
-  width: '100%';
   display: flex;
   justify-content: center;
   align-items: center;
   p {
     font-family: sans-serif;
-    font-size: 10rem;
+    font-size: 5vw;
     font-weight: bold;
     color: #4F81BD;
   }
